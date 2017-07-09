5fe6a82c4b429f45681a1c4a63027f99d8f8b6b1
Alexander Stokes <stokesbga@gmail.com>
2017-07-09 07:58:24 -0400

    pass page in onHasNoMoreData
    Page will allow logic for displaying NO data, or no MORE data messages

--- PaginatedListView.js
+++ PaginatedListView.js
@@ -92,7 +92,7 @@ class PaginatedListView extends React.Component {
             isFetching: false,
           });
         } else {
-          this.props.onHasNoMoreData();
+          this.props.onHasNoMoreData(this.state.page);
           this.setState({
             hasMoreData: false,
             isFetching: false,
